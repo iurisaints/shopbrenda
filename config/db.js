986f2c1c0daed1a8f569d78c85fab241d52f83d0
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2026-02-21 12:46:30 -0300

    testando db no deploy

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Cria um Pool de conexões (Melhor para produção que createConnection)
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'brenda_shop',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -15,5 +15,6 @@ const pool = mysql.createPool({
 const promisePool = pool.promise();
 
 console.log("--> Banco de Dados Configurado.");
+
 
 module.exports = promisePool;
