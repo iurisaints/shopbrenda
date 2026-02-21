7d61794f4b791fc17385e210d99f3fd6334b2174
iurisaints <iurisaints@gmail.com>
2026-02-21 14:47:35 -0300

    upload config

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,10 +1,18 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+const uploadDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 // Configuração de onde salvar
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, 'uploads/'),
-    filename: (req, file, cb) => {
+    destination: function (req, file, cb) {
+        cb(null, uploadDir); // 
+    },
+    filename: function (req, file, cb) {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
         cb(null, uniqueSuffix + path.extname(file.originalname));
     }
