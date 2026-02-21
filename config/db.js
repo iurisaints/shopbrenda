86f1277f4689bec1006e24e11e34060678e101f8
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2026-02-21 12:51:40 -0300

    Add direct connection test to database
    Added direct connection test to the database using getConnection.

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql2');
 
+
 // Cria um Pool de conexões (Melhor para produção que createConnection)
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -13,8 +14,17 @@ const pool = mysql.createPool({
 
 // Promisify para usar async/await
 const promisePool = pool.promise();
-
-console.log("--> Banco de Dados Configurado.");
+// TESTE DE CONEXÃO DIRETA PARA O RAILWAY
+db.getConnection()
+    .then(conn => {
+        console.log("🟢 BINGO! Conectado ao banco de dados com sucesso!");
+        conn.release();
+    })
+    .catch(err => {
+        console.log("🔴 ALERTA VERMELHO! O banco não conectou. Motivo:");
+        console.error(err);
+    });
 
 
 module.exports = promisePool;
+
